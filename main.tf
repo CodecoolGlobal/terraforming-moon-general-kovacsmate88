4b2a5cd5a369b0e3c4995f1dd5ec554f31d4b408
kovacsmate88 <kovacsmate321@gmail.com>
2023-05-03 14:13:44 +0200

    create an instance to connect only with my ssh key and only from my machine

---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name_prefix = "allow-ssh-"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["217.65.104.98/32"]
+  }
+}
+
+
+resource "aws_instance" "example" {
+  ami           = "ami-00aa9d3df94c6c354"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  key_name = "MYKEYEC2"
+
+  tags = {
+    Name = "My-Moon-EC2-insatnce"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+}
